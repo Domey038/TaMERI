90642be031ba0a9dbb1183769c317f7b79adc80e
Domey038 <domey038@live.de>
2018-07-10 17:57:53 +0200

    Divide code, automatic NN training and cleaing up

--- TaMERI/Main.py
+++ TaMERI/Main.py
@@ -24,23 +24,20 @@ mpg_dataset = IR.read_TestSet("data/mpg.tsv")
 import Preprocessing as PC
 x, y = PC.prepare_data(mpg_dataset)
 x = PC.one_hot_encoder(x, [6])
-x_train, x_test, y_train, y_test = PC.split_data(x,y)
-PC.create_fitting(x_train)
-x_train = PC.fit_data(x_train)
-x_test = PC.fit_data(x_test)
 
 #create the neural network
 import NeuralNetwork as NN
-NN.train_model(x_train, y_train)
-
-#use trained neural network to predict test data set
-predictions = NN.predict(x_test)
-
-#evaluate predictions
-real = y_test.tolist()
-for i, pred in enumerate(predictions):
-    print(str(pred) + "\t" + str(real[i]) + "\t" + str(pred-real[i]))
-
+NN.train_model(x,y)
+#NN.train_model(x_train, y_train, (6,6), 1000)
+#
+# #use trained neural network to predict test data set
+# predictions = NN.predict(x_test)
+#
 # #evaluate predictions
-# #print(confusion_matrix(y_test,predictions))
-# #print(classification_report(y_test,predictions))
+# real = y_test.tolist()
+# for i, pred in enumerate(predictions):
+#     print(str(pred) + "\t" + str(real[i]) + "\t" + str(pred-real[i]))
+#
+# # #evaluate predictions
+# # #print(confusion_matrix(y_test,predictions))
+# # #print(classification_report(y_test,predictions))
--- TaMERI/NeuralNetwork.py
+++ TaMERI/NeuralNetwork.py
@@ -1,10 +1,49 @@
 from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_error
+import Preprocessing as PC
 import pickle
 
-def train_model(training_data, training_result):
-    model = MLPRegressor(hidden_layer_sizes=(6,6),max_iter=10000)
-    model.fit(training_data, training_result)
-    pickle.dump(model, open("data/NN_model.pickle", 'wb'))
+def train_model(x, y):
+    min_MAE = 1000000000000000
+    best_hl = None
+    best_model = None
+    for i in range(1,20):
+        MAE, model = training_iteration(x, y, (i), 5000)
+        if MAE < min_MAE:
+            min_MAE = MAE
+            best_model = MAE
+            best_hl = i
+        print(str(i) + "\t" + str(MAE))
+    print("Single-layer best model: " + str(best_hl) + "\t" + str(min_MAE))
+
+    min_MAE = 1000000000000000
+    best_hl = None
+    best_model = None
+    for i in range(1,20):
+        for j in range(1,20):
+            MAE, model = training_iteration(x, y, (i,j), 5000)
+            if MAE < min_MAE:
+                min_MAE = MAE
+                best_model = MAE
+                best_hl = (i,j)
+            print(str(best_hl) + "\t" + str(MAE))
+    print("BI-layer best model: " + str(best_hl) + "\t" + str(min_MAE))
+
+def training_iteration(x, y, hls, iters):
+    #preprocessing (splitting and fitting)
+    x_train, x_test, y_train, y_test = PC.split_data(x,y)
+    PC.create_fitting(x_train)
+    x_train = PC.fit_data(x_train)
+    x_test = PC.fit_data(x_test)
+    #create the neural network
+    model = MLPRegressor(hidden_layer_sizes=hls, max_iter=iters)
+    model.fit(x_train, y_train)
+    #test model
+    pred = model.predict(x_test)
+    #evaluate result
+    MAE = mean_absolute_error(pred, y_test)
+    #return results
+    return MAE, model
 
 def predict(data):
     model = pickle.load(open("data/NN_model.pickle", 'rb'))
