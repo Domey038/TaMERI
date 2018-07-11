702dfb7444af6095cfa33e1565fb10a17c5b8226
Domey038 <domey038@live.de>
2018-07-11 16:27:42 +0200

    Automatic NN parameter calibration

--- TaMERI/Main.py
+++ TaMERI/Main.py
@@ -4,14 +4,46 @@
 #-----------------------------------------------------#
 #                   Library imports                   #
 #-----------------------------------------------------#
+import argparse
 import pandas as pd
 from sklearn.metrics import classification_report,confusion_matrix
 
 #-----------------------------------------------------#
 #                  Parse command line                 #
 #-----------------------------------------------------#
-#TODO: argparse
+#Initialize argument parser
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=
+    """TaMERI: TrAnsMembrane Evolutionary Rate Influence - Neural network predictor
+Prediction of transmembrane influence on the evolutionary rate of membrane proteins through protein determinants.
+Trained on AAIMON slopes from the Korbinian pipeline as representative for TM/EM ER ratios.
 
+Author: Dominink Müller
+Email: ga37xiy@tum.de
+Lab: Frishman lab - TUM Weihenstephan (Germany)
+""")
+#Add arguments
+parser.add_argument('-i', '--input', type=str, action='store', required=False, dest='args_input',
+                    help='Path to a UniProt flat file in txt format')
+parser.add_argument('-t', '--train', type=str, action='store', required=False, dest='args_train',
+                    help='Path to a training data set')
+parser.add_argument('-c', '--calibrate', default=False, action='store_true', dest='args_calibration',
+                    help='Boolean tag if the neural network parameters should be automatically calibrated depending on the\
+                    provided training data set or fixed parameters should be used\
+                    (which was selected by me as the best for this problem)')
+#parser.add_argument('-o', '--filesystem', type=str, action='store', required=True, dest='args_filesystem',
+#                    help='REQUIRED: Path where the filesystem (output) should be set up / stored (PDB database, results, temporary files, ...)')
+#Parse arguments
+args = parser.parse_args()
+
+#-----------------------------------------------------#
+#                     Parameters                      #
+#-----------------------------------------------------#
+#Path to input data for which predictions should be calculated
+path_inputData = args.args_input
+#Path to training data for which a own neural network model can be trained
+path_trainingData = args.args_train
+#Boolean tag, if the neural network parameter should be automatically calibrated depending on the training data set
+boolean_calibration = args.args_calibration
 
 #-----------------------------------------------------#
 #                    Runner code                      #
@@ -27,7 +59,10 @@ x = PC.one_hot_encoder(x, [6])
 
 #create the neural network
 import NeuralNetwork as NN
-NN.train_model(x,y)
+#NN.train_model(x,y)
+
+NN.calibrate_model_parameter(x,y)
+
 #NN.train_model(x_train, y_train, (6,6), 1000)
 #
 # #use trained neural network to predict test data set
--- TaMERI/NeuralNetwork.py
+++ TaMERI/NeuralNetwork.py
@@ -3,6 +3,23 @@ from sklearn.metrics import mean_absolute_error
 import Preprocessing as PC
 import pickle
 
+def calibrate_model_parameter(x, y):
+    from sklearn.model_selection import GridSearchCV
+    model = MLPRegressor(max_iter=10000)
+    # param_grid = [{'activation':['identity', 'logistic', 'tanh', 'relu'],
+    #             'solver':['lbfgs', 'sgd', 'adam'],
+    #             'hidden_layer_sizes':[(1,),(2,),(3,),(4,),(5,),(6,),(7,),(8,),(9,),(10,),(11,),
+    #             (12,),(13,),(14,),(15,),(16,),(17,),(18,),(19,),(20,)]
+    #             }]
+    param_grid = [{'activation':['identity', 'logistic', 'tanh', 'relu'],
+                'solver':['lbfgs', 'adam'],
+                'hidden_layer_sizes':[(1), (5), (10), (15), (20), (25), (30)]}]
+    clf = GridSearchCV(model, param_grid, cv=3, scoring='neg_mean_absolute_error')
+    clf.fit(x,y)
+
+    print("Best parameters set found on development set:")
+    print(clf.best_params_)
+
 def train_model(x, y):
     min_MAE = 1000000000000000
     best_hl = None
