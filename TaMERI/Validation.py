17734bc222317142d73797681eb485d805594cb5
Domey038 <domey038@live.de>
2018-07-26 16:45:42 +0200

    Improved automatic validation process

--- TaMERI/Validation.py
+++ TaMERI/Validation.py
@@ -2,6 +2,8 @@
 #                   Library imports                   #
 #-----------------------------------------------------#
 #External libraries
+import os
+import subprocess
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import explained_variance_score
@@ -21,6 +23,8 @@ def cross_validation(set_x, set_y, validation_ML_algorithm):
 
     #Iterate through each fold of the cross-validation
     current_fold = 1
+    outcome_data = []
+    outcome_results = []
     for train, test in folded_indices:
         #initialize the training and testing data sets according to fold indices
         train_x, test_x = set_x.iloc[train], set_x.iloc[test]
@@ -42,9 +46,15 @@ def cross_validation(set_x, set_y, validation_ML_algorithm):
         print("Fold " + str(current_fold) + " - " + "MAE: " + str(MAE))
         print("Fold " + str(current_fold) + " - " + "EVS: " + str(EVS))
         print("Fold " + str(current_fold) + " - " + "R2: " + str(R2))
-        #dump the predictions<->real of the current fold
-        dump_fold(predictions, test_y, current_fold)
+        #save the predictions<->real of the current fold
+        outcome_data.append((predictions, test_y, current_fold))
+        outcome_results.append((current_fold, MAE, EVS, R2))
+        #increase fold counter
         current_fold += 1
+    #Dump data and results into files
+    cv_results_dir = dump_results(outcome_data, outcome_results, validation_ML_algorithm)
+    #Plot evaluation results for visualization
+    plot_results(cv_results_dir, validation_ML_algorithm)
 
 #Evaluate the predictions by comparing to the real values
 def evaluate_predictions(pred, real):
@@ -53,10 +63,56 @@ def evaluate_predictions(pred, real):
     R2 = r2_score(real, pred)
     return MAE,EVS,R2
 
-#Dump the results of the current fold in a file
-def dump_fold(pred, real, fold_index):
-    with open("data/cv.fold_" + str(fold_index) + ".tsv", 'w') as fold_dumper:
-        fold_dumper.write("real" + "\t" + "pred" + "\n")
-        real = real.tolist()
-        for i, p in enumerate(pred):
-            fold_dumper.write(str(real[i]) + "\t" + str(p) + "\n")
+#Dump the prediction data and results into files
+def dump_results(data_list, result_list, validation_ML_algorithm):
+    #IF validation result directory doesn't exist -> create it
+    results_dir = os.path.join("data", "cv_results" + "." + validation_ML_algorithm)
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    #Dump cross validation data
+    cv_dataPATH = os.path.join(results_dir, "data.tsv")
+    with open(cv_dataPATH, 'w') as data_dumper:
+        #write header
+        data_dumper.write("real" + "\t" + "pred" + "\t" + "fold" + "\n")
+        #iterate over each saved fold_tuple
+        for fold_tuple in data_list:
+            #parse variables
+            pred = fold_tuple[0]
+            real = fold_tuple[1].tolist()
+            fold = fold_tuple[2]
+            #print data to file for each sample
+            for i, p in enumerate(pred):
+                data_dumper.write(str(real[i]) + "\t" + str(p) + "\t" + str(fold) + "\n")
+    #Dump cross validation results
+    cv_resultsPATH = os.path.join(results_dir, "scores.tsv")
+    with open(cv_resultsPATH, 'w') as result_dumper:
+        #write header
+        result_dumper.write("fold" + "\t" + "MAE" + "\t" + "EVS" + "\t" + "R2" + "\n")
+        #iterate over each saved fold_tuple
+        for fold_tuple in result_list:
+            #parse variables
+            fold = fold_tuple[0]
+            MAE = fold_tuple[1]
+            EVS = fold_tuple[2]
+            R2 = fold_tuple[3]
+            #print results to file for each sample
+            result_dumper.write(str(fold) + "\t" + str(MAE) + "\t" + str(EVS) + "\t" + str(R2) + "\n")
+    return results_dir
+
+#Plot evaluation via Rscript
+def plot_results(cv_results_dir, validation_ML_algorithm):
+    #Fixed parameter to Rscript
+    rscript_path = os.path.abspath(os.path.join("scripts", "evaluate_Validation.R"))
+    #Identify ML algorithm
+    ML_algorithm = None
+    if validation_ML_algorithm == "NN":
+        ML_algorithm = "Neural Network"
+    elif validation_ML_algorithm == "RF":
+        ML_algorithm = "Random Forest"
+    #Save and then change the current working directory into the cv_results directory
+    original_wd = os.getcwd()
+    os.chdir(cv_results_dir)
+    #Run the Rscript
+    out = subprocess.call(["Rscript", rscript_path, "./", ML_algorithm])
+    #Return to original working directory
+    os.chdir(original_wd)
--- scripts/evaluate_Validation.R
+++ scripts/evaluate_Validation.R
@@ -4,49 +4,31 @@ suppressMessages(library(magrittr))
 suppressMessages(library(tidyr))
 suppressMessages(library(ggplot2))
 suppressMessages(library(stringr))
-suppressMessages(library(gridExtra))
 
 #----------------------------------------#
 #             Preprocessing              #
 #----------------------------------------#
 #Commandline input
 args = commandArgs(trailingOnly=TRUE)
-if (length(args) != 1) {
+if (length(args) != 2) {
   print("")
-  stop("Required input:\n<TaMERI/data/>")
+  stop("Required input:\n<TaMERI/data/cv_results.X> <ML algorithm>")
 }
 
 #DEBUG - START
-args <- c("/home/domey/Studium-Shit/Master/SS_2018/Masterarbeit/working_dir/TaMERI/data/results_RF")
+#args <- c("/home/domey/Studium-Shit/Master/SS_2018/Masterarbeit/working_dir/TaMERI/data/results_RF")
 #DEBUG - END
 
 #Parameter
 data_set <- "homo_sapiens"
-ML_algorithm <- "Random Forest"
-
-#List all cross-validation folds in the data directory
-folds <- list.files(args[1], pattern="cv\\.fold_.*\\.tsv") %>%
-  strsplit(., "\\.") %>%
-  as.data.table(.) %>%
-  .[2,] %>%
-  unlist(.)
-
-#Create "plots" directory in working directory
-invisible(ifelse(!dir.exists("plots"), dir.create("plots"), FALSE))
+ML_algorithm <- args[2]
 
 #Initialize dt_complete (which contains the validation results of all cross-validation folds)
-cv_dt <- data.table()
+cv_dt_PATH <- file.path(args[1], "data.tsv")
+cv_dt <- fread(cv_dt_PATH, sep="\t", header=TRUE)
 
-#Combine the fold results into one table
-for (fold in folds){
-  #Read results
-  path <- file.path(args[1], paste("cv", fold, "tsv", sep="."))
-  results <- fread(path, sep="\t", header=TRUE)
-  #Add a fold column to the results table
-  results[, fold:=fold]
-  #Add to combined data frame
-  cv_dt <- rbind(cv_dt, results)
-}
+#Preprocessing
+cv_dt$fold = as.factor(cv_dt$fold)
 
 #Plot simple obs vs pred Scatterplot
 plotVS <- ggplot(cv_dt, aes(real, pred, col=fold)) + 
@@ -56,27 +38,26 @@ plotVS <- ggplot(cv_dt, aes(real, pred, col=fold)) +
   theme(legend.position="none") +
   xlab("TM/EM ER ratio - Observed") + 
   ylab("TM/EM ER ratio - Predicted") + 
-  ggtitle("5-fold cross-validation of TaMERI", 
+  ggtitle("Calibration scatterplot", 
           paste(paste("ML algorithm:", ML_algorithm, sep=" "),
                 paste("Data set:", data_set, sep=" "), sep="\n"
           ))
-outputPath = file.path("plots",
-                       paste("predictive_power", "scatterplot", "png", sep="."))
+outputPath = file.path(paste("predictive_power", "scatterplot", "png", sep="."))
 png(outputPath, 1600,1200, res=180)
 suppressWarnings(print(plotVS))
 out <- dev.off()
 
 #Preprocessing for trend accuracy
-cv_cons_dt <- cv_dt[, .(real_cons=real>0, pred_cons=pred>0, fold)]
-table(cv_cons_dt[,c("real_cons","pred_cons")])
+#cv_cons_dt <- cv_dt[, .(real_cons=real>0, pred_cons=pred>0, fold)]
+#table(cv_cons_dt[,c("real_cons","pred_cons")])
 
 #Preprocessing for accuracy vs cutoff
-ae <- cv_dt[, .(absolute_error=real-pred, fold=fold)]
+ae <- cv_dt[, .(absolute_error=abs(real-pred), fold=fold)]
 calc_accuracy <- function(cutoff, df){
   acc <- df[, .(accuracy=sum(absolute_error<=cutoff)/.N, abs_error_cutoff=cutoff), by=fold]
   return(acc)
 }
-diff_cutoffs <- seq(0, max(abs(ae$absolute_error)/2), max(abs(ae$absolute_error))/100)
+diff_cutoffs <- seq(0, 0.03, 0.03/100)
 acc_df <- lapply(diff_cutoffs, calc_accuracy, ae) %>%
   rbindlist(.)
 
@@ -86,24 +67,49 @@ plotAcc <- ggplot(acc_df, aes(abs_error_cutoff, accuracy, col=fold)) +
   geom_vline(xintercept=0.0025, linetype="dashed", col="black") + 
   scale_x_continuous(limits=c(0,0.03),
                      breaks = round(seq(0, 0.03, by = 0.0025),4)) + 
-  scale_y_continuous(limits=c(0.4,1.0),
-                     breaks = round(seq(min(acc_df$accuracy), 1.0, by = 0.05), 1)) +
+  scale_y_continuous(limits=c(0,1.0),
+                     breaks = round(seq(min(acc_df$accuracy), 1, by = 0.1), 1)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
   xlab("Absolute error cutoff") + 
   ylab("Accuracy") + 
-  ggtitle("5-fold cross-validation of TaMERI", 
+  ggtitle("Accuracy vs accepted abs error range", 
           paste(paste("ML algorithm:", ML_algorithm, sep=" "),
                 paste("Data set:", data_set, sep=" "), sep="\n"
                 ))
-outputPath = file.path("plots",
-                       paste("predictive_power", "accuracy", "png", sep="."))
+outputPath = file.path(paste("predictive_power", "accuracy", "png", sep="."))
 png(outputPath, 1600,1200, res=220)
 suppressWarnings(print(plotAcc))
 out <- dev.off()
 
-#Combine both plots into a single one
-outputPath = file.path("plots",
-                       paste("validation", "png", sep="."))
-png(outputPath, 1600,1600, res=220)
-suppressWarnings(grid.arrange(plotVS, plotAcc, heights=c(2,1)))
+#Error distribution - Histogram
+plotErr <- ggplot(ae, aes(absolute_error)) + 
+  geom_histogram(bins=100, col="black", fill=heat.colors(100)) + 
+  xlim(0, 0.025) + 
+  xlab("Absolute error") + 
+  ylab("Frequency") + 
+  ggtitle("Absolute error distribution", 
+          paste(paste("ML algorithm:", ML_algorithm, sep=" "),
+                paste("Data set:", data_set, sep=" "), sep="\n"
+          ))
+outputPath = file.path(paste("error_distribution", "png", sep="."))
+png(outputPath, 1600,1200, res=220)
+suppressWarnings(print(plotErr))
+out <- dev.off()
+
+#Error vs real - Scatterplot
+err_dt <- cbind(cv_dt, error=abs(cv_dt$real-cv_dt$pred))
+plotErrReal <- ggplot(err_dt, aes(real, error, col=fold)) + 
+  geom_point(size=0.75) + 
+  geom_hline(yintercept=0, linetype="dashed", size=0.25, col="black") + 
+  xlim(-0.025, 0.025) + 
+  ylim(0, 0.025) + 
+  xlab("Observed TM/EM ER ratio") + 
+  ylab("Absolute error") + 
+  ggtitle("Absolute error association to observations", 
+          paste(paste("ML algorithm:", ML_algorithm, sep=" "),
+                paste("Data set:", data_set, sep=" "), sep="\n"
+          ))
+outputPath = file.path(paste("error_vs_observed", "png", sep="."))
+png(outputPath, 1600,1200, res=220)
+suppressWarnings(print(plotErrReal))
 out <- dev.off()
