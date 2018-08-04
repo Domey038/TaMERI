9ba09c854135e90d1f1753904aed6079e9c58186
Domey038 <domey038@live.de>
2018-08-04 14:18:43 +0200

    Cross-validation result plot-script improvements

--- scripts/evaluate_Validation.R
+++ scripts/evaluate_Validation.R
@@ -16,7 +16,7 @@ if (length(args) != 2) {
 }
 
 #DEBUG - START
-#args <- c("/home/domey/Studium-Shit/Master/SS_2018/Masterarbeit/working_dir/TaMERI/data/results_RF")
+#args <- c("/home/domey/Studium-Shit/Master/SS_2018/Masterarbeit/working_dir/TaMERI/data/cv_results.RF")
 #DEBUG - END
 
 #Parameter
@@ -114,3 +114,62 @@ outputPath = file.path(paste("error_vs_observed", "png", sep="."))
 png(outputPath, 1600,1200, res=220)
 suppressWarnings(print(plotErrReal))
 out <- dev.off()
+
+
+#Error vs real - Scatterplot
+err_dt <- cbind(cv_dt, error=abs(cv_dt$real-cv_dt$pred))
+plotErrReal <- ggplot(err_dt, aes(real, error, col=fold)) + 
+  geom_point(size=0.75) + 
+  geom_hline(yintercept=0, linetype="dashed", size=0.25, col="black") + 
+  xlim(-0.025, 0.025) + 
+  ylim(0, 0.025) + 
+  xlab("Observed TM/EM ER ratio") + 
+  ylab("Absolute error") + 
+  ggtitle("Absolute error association to observations", 
+          paste(paste("ML algorithm:", ML_algorithm, sep=" "),
+                paste("Data set:", data_set, sep=" "), sep="\n"
+          ))
+outputPath = file.path(paste("error_vs_observed", "png", sep="."))
+png(outputPath, 1600,1200, res=220)
+suppressWarnings(print(plotErrReal))
+out <- dev.off()
+
+#Residual plot
+cv_dt[, residual:= real-pred]
+plotResiduals <- ggplot(cv_dt, aes(pred, residual, col="red")) + 
+  geom_point(size=0.75) + 
+  geom_smooth(method="lm", col="cyan", size=1) + 
+  geom_hline(yintercept=0, linetype="dashed", size=0.25, col="black") + 
+  xlab("Predicted TM/EM ER ratios") + 
+  ylab("Residuals") + 
+  theme(legend.position="none") + 
+  ggtitle("Residuals vs Fitted TM/EM ER ratios", 
+          paste(paste("ML algorithm:", ML_algorithm, sep=" "),
+                paste("Data set:", data_set, sep=" "), sep="\n"
+          ))
+outputPath = file.path(paste("residual_vs_fitted", "png", sep="."))
+png(outputPath, 1600,1200, res=220)
+suppressWarnings(print(plotResiduals))
+out <- dev.off()
+
+
+#Real AAIMON slope distribution
+real_quantiles <- quantile(cv_dt$real, probs=seq(0,1,0.05))
+plot_real_distribution <- ggplot(cv_dt, aes(real)) + 
+  geom_histogram(bins=100, col="black", fill=heat.colors(100)) + 
+  geom_vline(xintercept=0, linetype="dashed", size=0.75, col="black") + 
+  geom_vline(xintercept=real_quantiles[2], linetype="dashed", size=0.75, col="blue") + 
+  geom_vline(xintercept=real_quantiles[20], linetype="dashed", size=0.75, col="blue") + 
+  xlab("Korbinian TM/EM ER ratios") + 
+  ylab("Frequency") + 
+  scale_x_continuous(breaks = round(seq(min(cv_dt$real), max(cv_dt$real), by = 0.0025), 4)) +
+  theme(axis.text.x = element_text(angle = 60, hjust = 1)) + 
+  ggtitle("Korbinian TM/EM ER ratio distribution")
+outputPath = file.path(paste("residual_vs_fitted", "png", sep="."))
+png(outputPath, 1600,1200, res=220)
+suppressWarnings(print(plotResiduals))
+out <- dev.off()
+
+#Calculate value range
+value_range <- abs(real_quantiles[20]) + abs(real_quantiles[2])
+print(value_range * 10/100)
